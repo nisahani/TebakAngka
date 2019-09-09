4a6431b022c87038faf3dddd2f1cd27cf83e4dac
nisahani <nurrochmah.hani98@gmail.com>
2019-09-09 18:58:24 +0700

    TebakAngka

---
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/TebakAngka.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.tebakangka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TebakAngka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tebak_angka);
+    }
+}
